ce3c580a696cae363d569c6e2ea1193a46c9798d
Vyacheslav <vyacheslav1@ukr.net>
2020-12-04 01:31:21 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 const pool = new Pool(
     {
-        host: 'ec2-75-101-212-64.compute-1.amazonaws.com',
-        database: 'd5lba05geilg7',
-        user: 'ddameowovpwpgz',
+        host: '#',
+        database: '#',
+        user: '#',
         port: 5432,
-        password: '2856fef30fa4d831cd3ce4c79dc71b00176d145febdf48245cacb2a78eddd699'
+        password: '#'
     }
 );
 
